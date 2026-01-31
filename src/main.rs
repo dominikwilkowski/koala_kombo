7731402f7110d8f0fa59baa6984abc44a621acf7
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2026-02-01 06:27:26 +1000

    fixed log verbosity level

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use fyrox::engine::executor::Executor;
+use fyrox::{
+	engine::executor::Executor,
+	renderer::framework::core::log::{Log, MessageKind},
+};
 
 mod koala_kombo;
 
 fn main() {
+	Log::set_verbosity(MessageKind::Warning);
 	let mut executor = Executor::new();
 	executor.add_plugin(koala_kombo::GamePlugin::default());
 	executor.run();
